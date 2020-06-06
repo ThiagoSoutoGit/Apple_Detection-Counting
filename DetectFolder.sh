75782ac785420efee705f0e8726d22d376016884
Thiago Souto <thiago.souto@yahoo.com.br>
2020-06-07 10:29:32 +1200

    Apples Official

---
+++ DetectFolder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd AppleDetection
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29
+do
+   python detect.py --weights ./data/yolov4.weights --framework tf --size 608 --image ./data/$i.png
+
+done
+
---
+++ DownloadWeights.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd AppleDetection
+
+cd data
+
+wget https://drive.google.com/open?id=1cewMfusmPjYWbrnuJRuKhPMwRe_b9PaT
